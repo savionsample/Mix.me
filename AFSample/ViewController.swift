04cbdaa667f84bf63f178d6b8e8944d1366112bf
Savion <savsample@gmail.com>
2016-07-12 13:24:35 -0700

    Initial Commit

---
+++ AFSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AFSample
+//
+//  Created by Savion Sample on 7/12/16.
+//  Copyright © 2016 StereoLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
